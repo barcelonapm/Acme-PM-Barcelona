7d73ee513ee7a55a821124d1c1b126014a3ea026
Alex Muntada <alexm@alexm.org>
2009-09-02 01:04:46 +0200

    merge module-starter boilerplate

---
+++ lib/Acme/PM/Barcelona.pm
@@ -0,0 +1,107 @@
+package Acme::PM::Barcelona;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Acme::PM::Barcelona - The great new Acme::PM::Barcelona!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Acme::PM::Barcelona;
+
+    my $foo = Acme::PM::Barcelona->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alex Muntada, C<< <alexm at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-acme-pm-barcelona at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-PM-Barcelona>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Acme::PM::Barcelona
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-PM-Barcelona>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Acme-PM-Barcelona>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Acme-PM-Barcelona>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Acme-PM-Barcelona>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Alex Muntada, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Acme::PM::Barcelona
