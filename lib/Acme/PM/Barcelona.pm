4afc39ab66694c84c07809c52e4e08f1714c09e4
Alex Muntada <alexm@alexm.org>
2016-09-04 17:00:03 +0200

    Remove boilerplate and improve kwalitee

--- lib/Acme/PM/Barcelona.pm
+++ lib/Acme/PM/Barcelona.pm
@@ -1,20 +1,8 @@
-package Acme::PM::Barcelona;
-
 use warnings;
 use strict;
 
-=head1 NAME
-
-Acme::PM::Barcelona - Talks and projects by Barcelona.pm
-
-=head1 VERSION
-
-Version 0.04
-
-=cut
-
-our $VERSION = '0.04';
-
+package Acme::PM::Barcelona;
+# ABSTRACT: Talks and projects by Barcelona.pm
 
 =head1 SYNOPSIS
 
@@ -44,60 +32,9 @@ A way of finding out when the next meeting will take place.
 
 =back
 
-=head1 AUTHOR
-
-Alex Muntada, C<< <alexm at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-pm-barcelona at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-PM-Barcelona>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::PM::Barcelona
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-PM-Barcelona>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-PM-Barcelona>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-PM-Barcelona>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-PM-Barcelona>
-
-=back
-
-
 =head1 ACKNOWLEDGEMENTS
 
 Barcelona Perl Mongers
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Alex Muntada, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 
 =cut
 
--- lib/Acme/PM/Barcelona/Meeting.pm
+++ lib/Acme/PM/Barcelona/Meeting.pm
@@ -1,19 +1,8 @@
-package Acme::PM::Barcelona::Meeting;
-
 use warnings;
 use strict;
 
-=head1 NAME
-
-Acme::PM::Barcelona::Meeting - When is the next meeting?
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
+package Acme::PM::Barcelona::Meeting;
+# ABSTRACT: When is the next meeting?
 
 use base 'DateTime::Set';
 use DateTime;
@@ -59,60 +48,9 @@ sub new {
 
 =back
 
-=head1 AUTHOR
-
-Alex Muntada, C<< <alexm at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-acme-pm-barcelona at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-PM-Barcelona>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Acme::PM::Barcelona::Meeting
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-PM-Barcelona>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Acme-PM-Barcelona>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Acme-PM-Barcelona>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Acme-PM-Barcelona>
-
-=back
-
-
 =head1 ACKNOWLEDGEMENTS
 
 Barcelona Perl Mongers
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Alex Muntada, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 
 =cut
 
